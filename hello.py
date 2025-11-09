506d9186dd8d538a7ca940f544f8c3668a1e7fcf
prarthana-venugopal <vvce23cse0011@vvce.ac.in>
2025-11-09 14:30:31 +0530

    Changed greeting in master branch

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-print("Hello from the feature branch!")
+print("Hello from MASTER branch - updated message!")
 print("This is the demo file for Git concepts.")
 print("We will later create a merge conflict here.")
